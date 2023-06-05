477af4b7e98995cea25e35bd88502cdc4adc48ae
Chinar Dankhara <101128521+chinardankhara@users.noreply.github.com>
2023-06-05 11:37:19 -0400

    Create sakila-ex7.sql

---
+++ sakila-ex7.sql
@@ -0,0 +1,11 @@
+/*
+select substr('Please find the substring in this string', 17, 25-17+1)
+*/
+
+/*
+select abs(-25.76823), sign(-25.76823)
+*/
+
+/*
+select strftime('%m', date(unixepoch(), 'unixepoch'))
+*/
